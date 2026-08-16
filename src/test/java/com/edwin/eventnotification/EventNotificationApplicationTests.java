666d89911475b383719db1879d990100e761d202
Edwin <edwinvasa@gmail.com>
2026-08-16 10:52:18 -0500

    chore: initialize Spring Boot project

---
+++ src/test/java/com/edwin/eventnotification/EventNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.eventnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
